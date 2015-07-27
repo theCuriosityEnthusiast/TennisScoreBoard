e147d1a470e86df54b5d5299711e95d2dd5be6d2
theCuriosityEnthusiast <theCuriosityEnthusiast@users.noreply.github.com>
2015-07-27 10:08:59 +0530

    Update TennisScoreBoard.java
    added two counter variables and a constructor

--- src/TennisScoreBoard.java
+++ src/TennisScoreBoard.java
@@ -1,11 +1,12 @@
 
 public class TennisScoreBoard {
-	static final int DUO = -1;
-	static final int ADV = -2;
 	
-	int a = 0;
-	int b = 0;
+	private int scoreA;
+	private int scoreB;
 	
+	TennisScoreBoard(){
+		scoreA = 0;
+		scoreB = 0;
+	}
 	
-
 }
