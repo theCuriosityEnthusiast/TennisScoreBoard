a822b4d5cd7de10dfc8602e31b296cc8e7cdc03f
Artifacte <rajat.kulshreshtah50@gmail.com>
2015-07-27 10:13:29 +0530

    added method to take user input

---
+++ ScoreBoard.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ScoreBoard{
+	public static void main (String[] args) throws java.lang.Exception {
+		String userString = readUserInput();
+	}
+	
+	public static String readUserInput() {
+		Scanner reader = new Scanner (System.in);
+		return reader.nextLine();
+	}
+	
+}
