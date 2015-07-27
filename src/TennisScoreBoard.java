44a2cdbdef18f673a42616deb9c6cbe34a28c995
Abil N George <abilngeorge@gmail.com>
2015-07-27 11:11:01 +0530

    Fix:duceHandler

--- src/TennisScoreBoard.java
+++ src/TennisScoreBoard.java
@@ -1,49 +1,65 @@
 
 public class TennisScoreBoard {
 	
-	private int scoreA;
-	private int scoreB;
+	private static final int WINNING_DIFF = 2;
+	private static final int ADV_DIFF = 1;
+	private static final int DEUCE_POINT = 4;
+	
+	private static final String[] scoreStrings = {"0","15","30","40"};
+	
+	private int scorePlayerA;
+	private int scorePlayerB;
+
 	
 	TennisScoreBoard(){
-		scoreA = 0;
-		scoreB = 0;
+		scorePlayerA = 0;
+		scorePlayerB = 0;
 	}
 		
 	public String showScore() {
-		String[] scores = {"0","15","30","40"};
-		
-		if(scoreA >= 4 || scoreA >= 4) {
+
+		if(scorePlayerA >= DEUCE_POINT || scorePlayerA >= DEUCE_POINT ) {
 			 return dueHandler();
 		} else {
-			String scoreStringB = scores[scoreB];
-			String  scoreStringA = scores[scoreA];
+			String scoreStringB = scoreStrings[scorePlayerB];
+			String scoreStringA = scoreStrings[scorePlayerA];
 			return "Player 1: " + scoreStringA + " Player 2: " + scoreStringB ; 
 		}
 	}
 
 
 	private String dueHandler() {
-		if (scoreA == scoreB) {
-			return "DEUCE" ; 
-		} else if ( Math.abs(scoreA-scoreB) >= 2) {
+		
+		int currentDiff = Math.abs(scorePlayerA-scorePlayerB);
+		
+		if (currentDiff == 0) {		
 			
-			if(scoreA < scoreB) return "Player 1: _ Player 2: Game" ; 
-			else return "Player 1: Game Player 2: _" ; 
+			return "Player 1: 40 Player 2: 40" ;
 			
-		} else {
-			if(scoreA < scoreB) return "Player 1: 40 Player 2: Adv" ; 
+		} else if ( currentDiff == ADV_DIFF) {
+				
+			if(scorePlayerA < scorePlayerB) return "Player 1: 40 Player 2: Adv" ; 
 			else return "Player 1: Adv Player 2: 40" ; 
-		} 
+		
+		} else if ( currentDiff == WINNING_DIFF) { 
+			
+			if(scorePlayerA < scorePlayerB) return "Player 1: _ Player 2: Game" ; 
+			else return "Player 1: Game Player 2: _" ;
+			
+		} else {	
+			return "INVALID POINT SEQUENCE";
+		}
+		
 	}
 	
 
 	public void setScore(String pointSequence){
 		for(char ch: pointSequence.toCharArray()){
 			if(Character.toLowerCase(ch) == 'a'){
-				scoreA +=1;
+				scorePlayerA +=1;
 			}
 			else if(Character.toLowerCase(ch) == 'b'){
-				scoreB +=1;
+				scorePlayerB +=1;
 			}
 			else{
 				System.out.println("Invalid character");
