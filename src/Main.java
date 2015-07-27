a06603df8f8f5b6b42d0a2866bd143bb4183bd26
theCuriosityEnthusiast <theCuriosityEnthusiast@users.noreply.github.com>
2015-07-27 10:22:09 +0530

    Update Main.java
    Added input and output functionality and 2 new methods declared.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		String pointsSequence = input.nextInt();
+		
+		TennisScoreBoard wimbledonScores = new TennisScoreBoard();
+		wimbledonScores.setScore(pointsSequence);
+		wimbledonScores.showScore();
 	}
 
 }
